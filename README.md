000e201622d6b23bdf50da95b2bc13f2c58fb7e6
AxemaFr <axema@inbox.ru>
2020-12-05 12:58:52 +0300

    Day 4 - finished.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 | ✔ [Day 1: Report Repair](https://github.com/AxemaFr/AdventOfCode-2020/tree/master/day-01)| 🌟 | 🌟 |
 | ✔ [Day 2: Password Philosophy](https://github.com/AxemaFr/AdventOfCode-2020/tree/master/day-02)| 🌟 | 🌟 |
 | ✔ [Day 3: Toboggan Trajectory](https://github.com/AxemaFr/AdventOfCode-2020/tree/master/day-03)| 🌟 | 🌟 |
-| [Day 4: TBD]()| | |
+| ✔ [Day 4: Passport Processing]()| 🌟 | 🌟 |
 | [Day 5: TBD]()| | |
 | [Day 6: TBD]()| | |
 | [Day 7: TBD]()| | |
---
+++ day-04/solution.js
@@ -0,0 +1,52 @@
+import {INPUT} from './input.js';
+
+const REQUIRED_FIELDS = [
+  'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'
+];
+
+const FIElDS_VALIDATORS = [
+  {key: 'byr', validate: (v) => v >= 1920 && v <= 2002},
+  {key: 'iyr', validate: (v) => v >= 2010 && v <= 2020},
+  {key: 'eyr', validate: (v) => v >= 2020 && v <= 2030},
+  {key: 'hgt', validate: (v) => {
+    const num = v.replace(/\D/g,'');
+    return v.match(new RegExp('cm|in')) && (v.includes('cm') ? num >= 150 && num <= 193 : num >= 59 && num <= 76);
+  }},
+  {key: 'hcl', validate: (v) => v.match(/^#[0-9A-F]{6}$/i)},
+  {key: 'ecl', validate: (v) => v.match(new RegExp(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].join('|'), 'g'))},
+  {key: 'pid', validate: (v) => !isNaN(v) && v.split('').length === 9},
+];
+
+export const problem1 = (passports) => {
+  const validPassports = [];
+
+  passports.forEach((passport) => {
+    if (REQUIRED_FIELDS.every((it) => passport.indexOf(it) !== -1)) {
+      validPassports.push(passport);
+    }
+  });
+
+  return validPassports;
+};
+
+export const problem2 = (passports) => {
+  const validPassports = problem1(passports);
+
+  return validPassports.filter((passport) => {
+    const passportItems = passport.split(new RegExp(['\n', ' '].join('|'), 'g')).map((it) => it.split(':'));
+
+    return FIElDS_VALIDATORS.every((validator) => {
+      return validator.validate(passportItems.find((item) => item[0] === validator.key)[1]);
+    });
+  });
+};
+
+export const parseInput = (inputRaw) => {
+  return inputRaw.split('\n\n').map(parseSingleInput);
+};
+
+const parseSingleInput = (inputStr) => {
+  return inputStr;
+};
+
+
